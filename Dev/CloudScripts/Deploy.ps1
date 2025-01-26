80d3220c365b93b63c8c3e0af86b31b4d9ed4520
capacity-tighe-begley <tighe.begley@capacity.com>
2025-01-26 14:58:57 -0600

    fixed deploy

--- Dev/CloudScripts/Deploy.ps1
+++ Dev/CloudScripts/Deploy.ps1
@@ -60,7 +60,84 @@ $ScriptVersion = '25.1.22.1'
     #endregion
 
 iex (irm functions.tighenet.com)
+#region functions
+<#
+function Set-SetupCompleteCreateStartHOPEonUSB {
+    
+    $OSDCloudUSB = Get-Volume.usb | Where-Object {($_.FileSystemLabel -match 'OSDCloud') -or ($_.FileSystemLabel -match 'BHIMAGE')} | Select-Object -First 1
+    $SetupCompletePath = "$($OSDCloudUSB.DriveLetter):\OSDCloud\Config\Scripts\SetupComplete"
+    $ScriptsPath = $SetupCompletePath
 
+    if (!(Test-Path -Path $ScriptsPath)){New-Item -Path $ScriptsPath -ItemType Directory -Force}
+
+    $RunScript = @(@{ Script = "SetupComplete"; BatFile = 'SetupComplete.cmd'; ps1file = 'SetupComplete.ps1';Type = 'Setup'; Path = "$ScriptsPath"})
+
+
+    Write-Output "Creating $($RunScript.Script) Files"
+
+    $BatFilePath = "$($RunScript.Path)\$($RunScript.batFile)"
+    $PSFilePath = "$($RunScript.Path)\$($RunScript.ps1File)"
+            
+    #Create Batch File to Call PowerShell File
+    if (Test-Path -Path $PSFilePath){
+        copy-item $PSFilePath -Destination "$ScriptsPath\SetupComplete.ps1.bak"
+    }        
+    New-Item -Path $BatFilePath -ItemType File -Force
+    $CustomActionContent = New-Object system.text.stringbuilder
+    [void]$CustomActionContent.Append('%windir%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -File C:\OSDCloud\Scripts\SetupComplete\SetupComplete.ps1')
+    Add-Content -Path $BatFilePath -Value $CustomActionContent.ToString()
+
+    #Create PowerShell File to do actions
+
+    New-Item -Path $PSFilePath -ItemType File -Force
+    Add-Content -path $PSFilePath "Write-Output 'Starting SetupComplete HOPE Script Process'"
+    Add-Content -path $PSFilePath "Write-Output 'iex (irm deploy.tighenet.com)'"
+    Add-Content -path $PSFilePath 'if ((Test-WebConnection) -ne $true){Write-error "No Internet, Sleeping 2 Minutes" ; start-sleep -seconds 120}'
+    Add-Content -path $PSFilePath 'iex (irm deploy.tighenet.com)'
+}
+
+Function Restore-SetupCompleteOriginal {
+    $OSDCloudUSB = Get-Volume.usb | Where-Object {($_.FileSystemLabel -match 'OSDCloud') -or ($_.FileSystemLabel -match 'BHIMAGE')} | Select-Object -First 1
+    $SetupCompletePath = "$($OSDCloudUSB.DriveLetter):\OSDCloud\Config\Scripts\SetupComplete"
+    $ScriptsPath = $SetupCompletePath
+    if (Test-Path -Path "$ScriptsPath\SetupComplete.ps1.bak"){
+        copy-item -Path "$ScriptsPath\SetupComplete.ps1.bak" -Destination "$ScriptsPath\SetupComplete.ps1"
+    }
+}
+#>
+
+function Create-SetupCompleteOSDCloudFiles{
+    
+    $SetupCompletePath = "C:\OSDCloud\Scripts\SetupComplete"
+    $ScriptsPath = $SetupCompletePath
+
+    if (!(Test-Path -Path $ScriptsPath)){New-Item -Path $ScriptsPath -ItemType Directory -Force | Out-Null}
+
+    $RunScript = @(@{ Script = "SetupComplete"; BatFile = 'SetupComplete.cmd'; ps1file = 'SetupComplete.ps1';Type = 'Setup'; Path = "$ScriptsPath"})
+
+    Write-Output "Creating $($RunScript.Script) Files in $SetupCompletePath"
+
+    $BatFilePath = "$($RunScript.Path)\$($RunScript.batFile)"
+    $PSFilePath = "$($RunScript.Path)\$($RunScript.ps1File)"
+            
+    #Create Batch File to Call PowerShell File
+    if (Test-Path -Path $PSFilePath){
+        copy-item $PSFilePath -Destination "$ScriptsPath\SetupComplete.ps1.bak"
+    }        
+    New-Item -Path $BatFilePath -ItemType File -Force
+    $CustomActionContent = New-Object system.text.stringbuilder
+    [void]$CustomActionContent.Append('%windir%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -File C:\OSDCloud\Scripts\SetupComplete\SetupComplete.ps1')
+    Add-Content -Path $BatFilePath -Value $CustomActionContent.ToString()
+
+    #Create PowerShell File to do actions
+
+    New-Item -Path $PSFilePath -ItemType File -Force
+    Add-Content -path $PSFilePath "Write-Output 'Starting SetupComplete HOPE Script Process'"
+    Add-Content -path $PSFilePath "Write-Output 'iex (irm deploy.tighenet.com)'"
+    Add-Content -path $PSFilePath 'if ((Test-WebConnection) -ne $true){Write-error "No Internet, Sleeping 2 Minutes" ; start-sleep -seconds 120}'
+    Add-Content -path $PSFilePath 'iex (irm deploy.tighenet.com)'
+}
+#endregion
 if ($env:SystemDrive -eq 'X:') {
     $LogName = "Hope-$((Get-Date).ToString('yyyy-MM-dd-HHmmss')).log"
     Start-Transcript -Path $env:TEMP\$LogName -Append -Force
@@ -68,6 +145,13 @@ if ($env:SystemDrive -eq 'X:') {
 Write-SectionHeader -Message "Starting $ScriptName $ScriptVersion"
 write-host "Added Function Create-SetupCompleteOSDCloudFiles" -ForegroundColor Green
 
+
+<#
+if ($env:SystemDrive -ne 'X:') {
+    Write-Host -ForegroundColor Yellow "Restart after Script Completes?"
+    $Restart = Read-Host "y or n, then Enter"
+}
+#>
 
 
 Set-ExecutionPolicy Bypass -Force
@@ -93,7 +177,7 @@ if ($env:SystemDrive -eq 'X:') {
     iex (irm win11.tighenet.com)
 
     #Create Marker so it knows this is a "HOPE" computer - No longer need thanks to the custom setup complete above.
-    #new-item -Path C:\OSDCloud\configs -Name deploy.JSON -ItemType file
+    #new-item -Path C:\OSDCloud\configs -Name hope.JSON -ItemType file
     #Restore-SetupCompleteOriginal
     
     #Just go ahead and create the Setup Complete files on the C Drive in the correct Location now that OSDCloud is done in WinPE
@@ -113,7 +197,7 @@ if ($env:SystemDrive -ne 'X:') {
     Set-ExecutionPolicy Bypass -Force
 
     #Setup Post Actions Scheduled Task
-    iex (irm "postactiontask.tighenet.com")
+    iex (irm "https://raw.githubusercontent.com/gwblok/garytown/master/Dev/CloudScripts/PostActionsTask.ps1")
 
     #Disable Auto Bitlocker
     New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\BitLocker -Name PreventDeviceEncryption -PropertyType dword -Value 1 -Force
@@ -124,7 +208,15 @@ if ($env:SystemDrive -ne 'X:') {
         iex (irm https://raw.githubusercontent.com/suazione/CodeDump/main/Set-ConfigureChatAutoInstall.ps1)
     }
     catch {}
-    
+    # Add Hope PDF to Desktop
+    Write-Host -ForegroundColor Gray "**Adding HOPE PDF to Desktop**" 
+    try {
+        Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/gwblok/garytown/85ad154fa2964ea4757a458dc5c91aea5bf483c6/HopeForUsedComputers/Hope%20for%20Used%20Computers%20PDF.pdf" -OutFile "C:\Users\Public\Desktop\Hope For Used Computers.pdf"
+    }
+    catch {}
+
+    #Set DO
+    #Set-DOPoliciesGPORegistry
     
     Write-SectionHeader -Message "**Running Test.tighenet.com**" 
     iex (irm test.tighenet.com)
@@ -139,47 +231,47 @@ if ($env:SystemDrive -ne 'X:') {
     #Enable "Notify me when a restart is required to finish updating"
     New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings -Name RestartNotificationsAllowed2 -PropertyType dword -Value 1
 
-    # Write-SectionHeader -Message  "**Setting Default Profile Personal Preferences**" 
-    # Set-DefaultProfilePersonalPref
+    Write-SectionHeader -Message  "**Setting Default Profile Personal Preferences**" 
+    Set-DefaultProfilePersonalPref
     
     #Try to prevent crap from auto installing
     Write-Host -ForegroundColor Gray "**Disabling Cloud Content**" 
     Disable-CloudContent
     
-    # #Set Win11 Bypasses
-    # Write-Host -ForegroundColor Gray "**Enabling Win11 Bypasses**" 
-    # Set-Win11ReqBypassRegValues
+    #Set Win11 Bypasses
+    Write-Host -ForegroundColor Gray "**Enabling Win11 Bypasses**" 
+    Set-Win11ReqBypassRegValues
     
-    # #Windows Updates
-    # Write-SectionHeader -Message "**Running MS Updates**"
-    # Write-Host -ForegroundColor Gray "**Running Defender Updates**"
-    # Update-DefenderStack
-    # Write-Host -ForegroundColor Gray "**Running Windows Updates**"
-    # Start-WindowsUpdate
-    # Write-Host -ForegroundColor Gray "**Running Driver Updates**"
-    # Start-WindowsUpdateDriver
+    #Windows Updates
+    Write-SectionHeader -Message "**Running MS Updates**"
+    Write-Host -ForegroundColor Gray "**Running Defender Updates**"
+    Update-DefenderStack
+    Write-Host -ForegroundColor Gray "**Running Windows Updates**"
+    Start-WindowsUpdate
+    Write-Host -ForegroundColor Gray "**Running Driver Updates**"
+    Start-WindowsUpdateDriver
 
-    # #Store Updates
-    # Write-Host -ForegroundColor Gray "**Running Winget Updates**"
-    # Write-Host -ForegroundColor Gray "Invoke-UpdateScanMethodMSStore"
-    # Invoke-UpdateScanMethodMSStore
-    # #Write-Host -ForegroundColor Gray "winget upgrade --all --accept-package-agreements --accept-source-agreements"
-    # #winget upgrade --all --accept-package-agreements --accept-source-agreements
+    #Store Updates
+    Write-Host -ForegroundColor Gray "**Running Winget Updates**"
+    Write-Host -ForegroundColor Gray "Invoke-UpdateScanMethodMSStore"
+    Invoke-UpdateScanMethodMSStore
+    #Write-Host -ForegroundColor Gray "winget upgrade --all --accept-package-agreements --accept-source-agreements"
+    #winget upgrade --all --accept-package-agreements --accept-source-agreements
 
     #Modified Version of Andrew's Debloat Script
     Write-SectionHeader -Message "**Running Debloat Script**" 
-    iex (irm debloat.tighenet.com)
+    iex (irm https://raw.githubusercontent.com/gwblok/garytown/master/Dev/CloudScripts/Debloat.ps1)
 
-    # #OEM Updates
-    # try {
-    #     iex (irm https://raw.githubusercontent.com/gwblok/garytown/master/Dev/CloudScripts/LenovoUpdate.ps1)
-    # }
-    # catch {}
+    #OEM Updates
+    try {
+        iex (irm https://raw.githubusercontent.com/gwblok/garytown/master/Dev/CloudScripts/LenovoUpdate.ps1)
+    }
+    catch {}
 
-    # try {
-    #     iex (irm https://dell.tighenet.com)
-    # }
-    # catch {}
+    try {
+        iex (irm https://dell.tighenet.com)
+    }
+    catch {}
 
     #Set Time Zone
     Write-Host -ForegroundColor Gray "**Setting TimeZone based on IP**"
